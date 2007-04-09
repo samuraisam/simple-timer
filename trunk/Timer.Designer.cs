351c7ad0a128bb1dcffa14e82a964c8f429023e8
samuraiblog <samuraiblog@06f49d70-c32c-0410-9223-3d75a7a18c28>
2007-04-09 22:32:28 +0000

    Added the ability to keep track of the timer's start/stop via a slide-down grid view.

--- trunk/Timer.Designer.cs
+++ trunk/Timer.Designer.cs
@@ -36,6 +36,12 @@ namespace Timer
             this.resetButton = new System.Windows.Forms.Button();
             this.exitButton = new System.Windows.Forms.Button();
             this.renameButton = new System.Windows.Forms.Button();
+            this.timerLog = new System.Windows.Forms.DataGridView();
+            this.showLog = new System.Windows.Forms.Button();
+            this.startColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stopColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.timerLog)).BeginInit();
             this.SuspendLayout();
             // 
             // timerMain
@@ -60,10 +66,10 @@ namespace Timer
             this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startButton.Location = new System.Drawing.Point(12, 59);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(72, 38);
+            this.startButton.Size = new System.Drawing.Size(91, 38);
             this.startButton.TabIndex = 1;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = false;
@@ -77,7 +83,7 @@ namespace Timer
             this.resetButton.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resetButton.Location = new System.Drawing.Point(90, 59);
+            this.resetButton.Location = new System.Drawing.Point(109, 59);
             this.resetButton.Name = "resetButton";
             this.resetButton.Size = new System.Drawing.Size(55, 38);
             this.resetButton.TabIndex = 2;
@@ -93,10 +99,10 @@ namespace Timer
             this.exitButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.exitButton.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.exitButton.Location = new System.Drawing.Point(231, 59);
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(250, 59);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(66, 38);
+            this.exitButton.Size = new System.Drawing.Size(47, 38);
             this.exitButton.TabIndex = 3;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = false;
@@ -110,7 +116,7 @@ namespace Timer
             this.renameButton.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.renameButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.renameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.renameButton.Location = new System.Drawing.Point(151, 59);
+            this.renameButton.Location = new System.Drawing.Point(170, 59);
             this.renameButton.Name = "renameButton";
             this.renameButton.Size = new System.Drawing.Size(74, 38);
             this.renameButton.TabIndex = 4;
@@ -118,13 +124,72 @@ namespace Timer
             this.renameButton.UseVisualStyleBackColor = false;
             this.renameButton.Click += new System.EventHandler(this.renameButton_Click);
             // 
+            // timerLog
+            // 
+            this.timerLog.AllowUserToAddRows = false;
+            this.timerLog.AllowUserToDeleteRows = false;
+            this.timerLog.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.timerLog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.timerLog.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.startColumn,
+            this.stopColumn,
+            this.timeColumn});
+            this.timerLog.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.timerLog.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.timerLog.Location = new System.Drawing.Point(13, 126);
+            this.timerLog.Name = "timerLog";
+            this.timerLog.ReadOnly = true;
+            this.timerLog.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.timerLog.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.timerLog.ShowEditingIcon = false;
+            this.timerLog.ShowRowErrors = false;
+            this.timerLog.Size = new System.Drawing.Size(284, 210);
+            this.timerLog.TabIndex = 5;
+            // 
+            // showLog
+            // 
+            this.showLog.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.showLog.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.showLog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.showLog.Image = global::Timer.Properties.Resources.showLogDown;
+            this.showLog.Location = new System.Drawing.Point(12, 103);
+            this.showLog.Name = "showLog";
+            this.showLog.Size = new System.Drawing.Size(285, 15);
+            this.showLog.TabIndex = 6;
+            this.showLog.UseVisualStyleBackColor = false;
+            this.showLog.Click += new System.EventHandler(this.showLog_Click);
+            // 
+            // startColumn
+            // 
+            this.startColumn.HeaderText = "Start";
+            this.startColumn.Name = "startColumn";
+            this.startColumn.ReadOnly = true;
+            this.startColumn.Width = 80;
+            // 
+            // stopColumn
+            // 
+            this.stopColumn.HeaderText = "Stop";
+            this.stopColumn.Name = "stopColumn";
+            this.stopColumn.ReadOnly = true;
+            this.stopColumn.Width = 80;
+            // 
+            // timeColumn
+            // 
+            this.timeColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.timeColumn.HeaderText = "Time";
+            this.timeColumn.Name = "timeColumn";
+            this.timeColumn.ReadOnly = true;
+            // 
             // Timer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.exitButton;
-            this.ClientSize = new System.Drawing.Size(309, 110);
+            this.ClientSize = new System.Drawing.Size(309, 126);
             this.ControlBox = false;
+            this.Controls.Add(this.showLog);
+            this.Controls.Add(this.timerLog);
             this.Controls.Add(this.renameButton);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.resetButton);
@@ -135,6 +200,7 @@ namespace Timer
             this.Name = "Timer";
             this.Opacity = 0.95;
             this.Text = "Timer";
+            ((System.ComponentModel.ISupportInitialize)(this.timerLog)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,6 +214,11 @@ namespace Timer
         private System.Windows.Forms.Button resetButton;
         private System.Windows.Forms.Button exitButton;
         private System.Windows.Forms.Button renameButton;
+        private System.Windows.Forms.DataGridView timerLog;
+        private System.Windows.Forms.Button showLog;
+        private System.Windows.Forms.DataGridViewTextBoxColumn startColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stopColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn timeColumn;
     }
 }
 
