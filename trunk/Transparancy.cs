e0f6b115b7bc7302d70d4f9f71b68a5fe6bc5d6a
samuraiblog <samuraiblog@06f49d70-c32c-0410-9223-3d75a7a18c28>
2007-04-15 00:20:07 +0000

    Made transparency changer modaless.

--- trunk/Transparancy.cs
+++ trunk/Transparancy.cs
@@ -17,18 +17,23 @@ namespace Timer
             InitializeComponent();
         }
 
-        public DialogResult ShowCustomDialog(Timer timer)
+        public void ShowCustomDialog(Timer timer)
         {
             this.ParentTimer = timer;
             this.Opacity = timer.Opacity;
             this.trackBar1.Value = Convert.ToInt32(timer.Opacity * 100);
-            return this.ShowDialog();
+            this.Show();
         }
 
         private void trackBar1_ValueChanged(object sender, EventArgs e)
         {
             this.ParentTimer.Opacity = ((double)this.trackBar1.Value) * 0.01;
             this.Opacity = this.ParentTimer.Opacity;
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
